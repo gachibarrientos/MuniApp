17aefc8ff6ee19dc1c53f0c155477569ced31985
Gabriel Barrientos <gabrielb_31@hotmail.com>
2016-09-18 16:30:44 -0300

    Creación del Scrolling layout About US

---
+++ app/src/main/java/generic/muniapp/Solicitud.java
@@ -0,0 +1,13 @@
+package generic.muniapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Solicitud extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solicitud);
+    }
+}
